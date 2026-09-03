15ce5aad911fcbd69686cc04fa8c356b24293171
luiz-henrique-gomes-lima <luizhenrique.gomes.l.lima@gmail.com>
2026-09-03 09:15:05 -0300

    Add files via upload

---
+++ Aula_01_Web_site.vb
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang=" pt-br">
+
+
+<head>
+    <meta content ="text/html"  charset = "utf-8">
+<title> Aula de Desenv Front End - 13/08</title>
+</head>
+
+<body>
+    <h1>Aula 1 - Desenvolvimento Front End </h1>
+    <h2>Data: 13/08/2026 </h2>
+    <br/>
+    <hr/>
+    <p>
+        Este é um novo paragrafo. Vamos demonstrar como pode haver diferentes tags de HTML5 , e como trabalham.
+    </p>
+    <div>
+        Este é uma tag div. Ela é um bloco retangular onde podemos colocar texto, figuras e etc. Dentro dela vamos colocar esta tag span, de tipo inline que não quebra linha e nao é tag do tipo bloco.
+    </div>
+    <br>
+
+    <article>Esta é uma tag article. Dentro vamos colocar algo em <strong> Negrito</strong>, outro texto em <em> Itálico</em> o outro <u> Sublinhado </u> e o ultimo ficará <mark> o texto marcado </mark> </sub></article>
+
+    <h2> Lista de países</h2>
+
+    <ul>
+        <li>Canadá</li>
+        <li>Brazil</li>
+        <li>China</li>
+        <li>Portugal</li>
+        <li>Equador</li>
+    </ul>
+
+    <h2> Cantores famosos</h2>
+
+    <ol>
+        <li>Bruno</li>
+        <li>Justin Timberlake</li>
+        <li>Beyonce</li>
+        <li>Tim maia</li>
+        <li>Michael Jackson</li>
+    </ol>
+
+    <hr/>
+
+    <h2> Tabela com países e suas capitais </h2>
+        <br/>
+
+        <table border="1">
+            <tr>
+                <td><strong>Pais</strong></td>
+                <td><strong>Capital</strong></td>
+            </tr>
+            <tr>
+                <td>Brasil</td>     <td>Brasília</td>
+            </tr>
+             <tr>
+                <td>Argentina</td>     <td>Buenos Aires</td>
+            </tr>
+             <tr>
+                <td>Estados Unidos</td>     <td>Washington DC </td>
+            </tr>
+             <tr>
+                <td>Espanha </td>     <td>Madri</td>
+            </tr>
+        </table>
+
+            
+    <hr/>
+   
+
+    <img src="imagens/cachorro.jpg" alt="Cachorro" width="200"><img/>
+
+
+    <hr/>
+
+    <figure>
+        <img src="imagens/gato.jpg" alt="Gato">
+        <figcaption>Esse é um gato siames</figcaption>
+    </figcaption>
+
+    <hr/>
+    <br/>
+<p>Este site foi elaborado para a primeira aula de Desenvolvimento de Front end . Aluno: Luiz Henrique Gomes </p>
+
+
+
+
+
+</body>
+
+</html>
+
+
